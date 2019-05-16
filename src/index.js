3b418314ee8f90a3a21abdb08bf5bd6046dab065
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-16 11:02:54 -0400

    Unique Keys
    More Photos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+//Importing short id so that I can add unique keys to the photos
+import shortid from "shortid";
+
 class App extends React.Component {
   state = {
     urlArr: [],
@@ -12,7 +15,7 @@ class App extends React.Component {
   //Async means that the function will do some stuff at the same time, await tells the progam to chill out and focus on one task, can only use await in async functions
   async componentDidMount() {
     //Putting the Url in a variable
-    const url = "https://randomuser.me/api/";
+    const url = "https://randomuser.me/api/?results=100";
 
     //Need to use await to introduce promises, works as a replacement to .then
     const resource = await fetch(url);
@@ -30,10 +33,10 @@ class App extends React.Component {
 
     return (
       <div>
-        {/*Using map to itterate through the array of objects and pic out what I need which is the photos*/}
+        {/*Using map to itterate through the array of objects and pic out what I need which is the photos, Index is a simple way to add keys to the list, but shortid is better*/}
         {urlArr.map(resource => (
           <img
-            key={resource.id.value}
+            key={shortid.generate()}
             src={resource.picture.medium}
             alt="Loading"
           />
